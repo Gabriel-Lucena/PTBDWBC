b40b7cf436c4fba309942c2e1338841b007d4b82
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2024-03-29 15:07:07 -0300

    feat: :dizzy: Favicon aleatório
    Escolha de favicon aleatória implementada

--- menu/script.js
+++ menu/script.js
@@ -1,1 +1,13 @@
-console.log("teste");
+document.addEventListener('DOMContentLoaded', function () {
+  const favicons = [
+    'tumblr_inline_mgg2lh18is1roozkr540.gif',
+    'tumblr_inline_mgg2lnHQka1roozkr540.gif',
+    'tumblr_lqwdpnsIGo1qcfn0j540.gif',
+  ];
+
+  const randomFavicon = favicons[Math.floor(Math.random() * favicons.length)];
+  console.log(randomFavicon);
+
+  const faviconLink = document.getElementById('dynamic-favicon');
+  faviconLink.href = `./images/${randomFavicon}`;
+});
